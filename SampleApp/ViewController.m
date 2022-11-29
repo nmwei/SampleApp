3e04b249abb34e7d8cc7a491449e61df1f6a7d99
nmwei <254060001@qq.com>
2022-11-29 20:03:39 +0800

    子view是一个栈结构，后添加的view覆盖前添加的view

--- SampleApp/ViewController.m
+++ SampleApp/ViewController.m
@@ -16,13 +16,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    [self.view addSubview:({
-            UILabel *label = [[UILabel alloc] init];
-            label.text = @"hello world";
-            [label sizeToFit];
-            label.center = CGPointMake(self.view.frame.size.width/2, self.view.frame.size.height/2);
-            label;
-    })];
+    UIView *view = [[UIView alloc] init];
+    view.backgroundColor = [UIColor redColor];
+    view.frame = CGRectMake(100, 100, 100, 100);
+    [self.view addSubview:view];
+    
+    UIView *view2 = [[UIView alloc] init];
+    view2.backgroundColor = [UIColor blueColor];
+    view2.frame = CGRectMake(150, 150, 100, 100);
+    [self.view addSubview:view2];
 }
 
 
