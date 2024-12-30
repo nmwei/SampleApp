35b6826d1251d77d107e803e6e89a8d3983a89bb
nmwei <254060001@qq.com>
2024-12-30 20:24:45 +0800

    添加UITableView，定义行数及样式

--- SampleApp/ViewController.m
+++ SampleApp/ViewController.m
@@ -6,7 +6,7 @@
 //
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITableViewDataSource>
 
 @end
 
@@ -17,22 +17,23 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor whiteColor];
     
-    UIView *view = [[UIView alloc] init];
-    view.backgroundColor = [UIColor greenColor];
-    view.frame = CGRectMake(100, 100, 100, 100);
-    [self.view addSubview:view];
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
     
+    tableView.dataSource = self;
+    [self.view addSubview: tableView];
     
-    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action: @selector(pushController)]; //点击事件
-    [view addGestureRecognizer:tap]; //给view添加点击事件
 }
 
--(void) pushController {
-    UIViewController *viewController = [[UIViewController alloc] init];
-    viewController.view.backgroundColor = [UIColor purpleColor];
-    viewController.navigationItem.title = @"内容"; //设置标题
-    viewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"右侧标题" menu: nil];  //设置右侧按钮
-    [self.navigationController pushViewController:viewController animated:YES]; //路由栈添加
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:(UITableViewCellStyleSubtitle) reuseIdentifier:@"id"];
+    cell.textLabel.text = @"主标题";
+    cell.detailTextLabel.text = @"副标题";
+    return cell;
 }
 
 @end
