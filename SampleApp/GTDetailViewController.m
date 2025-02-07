802694ef052f9d934d74274f299ed2463d56e499
nmwei <254060001@qq.com>
2025-02-07 14:32:55 +0800

    使用navigationDelegate

--- SampleApp/GTDetailViewController.m
+++ SampleApp/GTDetailViewController.m
@@ -8,7 +8,7 @@
 #import "GTDetailViewController.h"
 #import <WebKit/WebKit.h>
 
-@interface GTDetailViewController ()
+@interface GTDetailViewController ()<WKNavigationDelegate>
 @property (nonatomic, strong, readwrite) WKWebView *webView;
 
 @end
@@ -19,11 +19,20 @@
     [super viewDidLoad];
     [self.view addSubview:({
         self.webView = [[WKWebView alloc] initWithFrame:CGRectMake(0, 88, self.view.frame.size.width, self.view.frame.size.height - 88)];
+        self.webView.navigationDelegate = self;
         self.webView;
     })];
 
     [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://time.geekbang.org/"]]];
     
+}
+
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+    decisionHandler(WKNavigationResponsePolicyAllow);
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation {
+    NSLog(@"webview加载完成");
 }
 
 @end
