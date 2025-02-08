fb9fbf3a4917b91448083ee5a1c9f0af87cebf34
nmwei <254060001@qq.com>
2025-02-08 14:38:13 +0800

    发请求获取数据

--- SampleApp/loader/GTListLoader.m
+++ SampleApp/loader/GTListLoader.m
@@ -10,11 +10,20 @@
 @implementation GTListLoader
 
 -(void) loadListData {
+    //创建url
     NSString *urlString = @"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
     NSURL *listURL = [NSURL URLWithString:urlString];
     
-    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+    //创建session
+    NSURLSession *session = [NSURLSession sharedSession]; //使用默认session
     
+    //创建sessionDataTask
+    NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSLog(@"");
+    }];
+    
+    //启动任务
+    [dataTask resume];
 }
 
 @end
