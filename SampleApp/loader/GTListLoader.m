4473257d7ecc58624407790111a67f6678c40676
nmwei <254060001@qq.com>
2025-02-08 11:38:54 +0800

    NSURLRequest使用

--- SampleApp/loader/GTListLoader.m
+++ SampleApp/loader/GTListLoader.m
@@ -12,6 +12,9 @@
 -(void) loadListData {
     NSString *urlString = @"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
     NSURL *listURL = [NSURL URLWithString:urlString];
+    
+    NSURLRequest *listRequest = [NSURLRequest requestWithURL:listURL];
+    
 }
 
 @end
