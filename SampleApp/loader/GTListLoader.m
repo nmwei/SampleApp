d03f107b4be6030fcdf3aa7997800d72726f2174
nmwei <254060001@qq.com>
2025-02-08 17:18:42 +0800

    代码还原，使用自己实现的请求

--- SampleApp/loader/GTListLoader.m
+++ SampleApp/loader/GTListLoader.m
@@ -13,25 +13,25 @@
 -(void) loadListData {
     NSString *urlString = @"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
     
-    [[AFHTTPSessionManager manager]GET:urlString parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
-        
-    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-        NSLog(@"");
-    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"");
-    }];
+//    [[AFHTTPSessionManager manager]GET:urlString parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+//
+//    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+//        NSLog(@"");
+//    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+//        NSLog(@"");
+//    }];
     
     // 创建url
     NSURL *listURL = [NSURL URLWithString:urlString];
-    
+
     //创建session
     NSURLSession *session = [NSURLSession sharedSession]; //使用默认session
-    
+
     //创建sessionDataTask
     NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         NSLog(@"");
     }];
-    
+
     //启动任务
     [dataTask resume];
 }
