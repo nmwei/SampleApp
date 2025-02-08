b1d1e77855cf0a985323325d608687e747511ed9
nmwei <254060001@qq.com>
2025-02-08 16:52:24 +0800

    使用AFNetworking三方库发请求

--- SampleApp/loader/GTListLoader.m
+++ SampleApp/loader/GTListLoader.m
@@ -6,12 +6,22 @@
 //
 
 #import "GTListLoader.h"
+#import "AFNetworking.h"
 
 @implementation GTListLoader
 
 -(void) loadListData {
-    //创建url
     NSString *urlString = @"https://static001.geekbang.org/univer/classes/ios_dev/lession/45/toutiao.json";
+    
+    [[AFHTTPSessionManager manager]GET:urlString parameters:nil headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        NSLog(@"");
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"");
+    }];
+    
+    // 创建url
     NSURL *listURL = [NSURL URLWithString:urlString];
     
     //创建session
