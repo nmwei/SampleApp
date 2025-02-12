7b6b64d534313bf717bbaf72bd5cb512cad639e2
nmwei <254060001@qq.com>
2025-02-12 17:59:06 +0800

    UISceneDelegate生命周期

--- SampleApp/SceneDelegate.m
+++ SampleApp/SceneDelegate.m
@@ -99,6 +99,7 @@
     NSLog(@"did selected");
 }
 
+// 连接到会话时
 - (void)sceneDidDisconnect:(UIScene *)scene {
     // Called as the scene is being released by the system.
     // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -106,21 +107,26 @@
     // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
 }
 
+// 后台 → 前台
 - (void)sceneDidBecomeActive:(UIScene *)scene {
     // Called when the scene has moved from an inactive state to an active state.
     // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
 }
 
+// 后台 → 前台
 - (void)sceneWillResignActive:(UIScene *)scene {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
 }
 
+// 后台 → 前台
 - (void)sceneWillEnterForeground:(UIScene *)scene {
     // Called as the scene transitions from the background to the foreground.
     // Use this method to undo the changes made on entering the background.
 }
 
+
+// 前台 → 后台
 - (void)sceneDidEnterBackground:(UIScene *)scene {
     // Called as the scene transitions from the foreground to the background.
     // Use this method to save data, release shared resources, and store enough scene-specific state information
