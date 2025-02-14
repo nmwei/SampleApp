cbd8e318a47982c41c6c7d133f70725c4ad2eada
nmwei <254060001@qq.com>
2025-02-14 11:46:32 +0800

    安装CocoaLumberjack

--- Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-resources.sh
+++ Pods/Target Support Files/Pods-SampleApp/Pods-SampleApp-resources.sh
@@ -97,9 +97,11 @@ EOM
   esac
 }
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/CocoaLumberjack/CocoaLumberjackPrivacy.bundle"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/SDWebImage/SDWebImage.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_resource "${PODS_CONFIGURATION_BUILD_DIR}/CocoaLumberjack/CocoaLumberjackPrivacy.bundle"
   install_resource "${PODS_CONFIGURATION_BUILD_DIR}/SDWebImage/SDWebImage.bundle"
 fi
 
